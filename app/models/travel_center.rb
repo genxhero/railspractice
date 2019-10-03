cdaf8e21bba5fc68c777a1015b2b1203a7bc030a
genxhero <genxhero@users.noreply.github.com>
2019-10-03 10:54:11 -0700

    add associations for travelstops

--- app/models/travel_center.rb
+++ app/models/travel_center.rb
@@ -1,2 +1,10 @@
 class TravelCenter < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: { scope: :location,
+    message: "record already exists for #{name} in #{:location.name}" }
+
+    belongs_to :location,
+    primary_key :id, 
+    foreign_key :location_id,
+    class_name "Location"
 end
