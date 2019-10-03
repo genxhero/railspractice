bee6cc8e0f8b2e15395148cd5bd2c16967049b12
genxhero <genxhero@users.noreply.github.com>
2019-10-03 10:25:52 -0700

    validations for location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ApplicationRecord
+    validates :name, presence: true
+    validates :state, presence: true
+    validates :zip, presence: true
+    validates :name, uniqueness:  { scope: :zip,
+    message: "only one town exists by that name within #{zip}" }
 end
--- db/migrate/20191003170132_create_locations.rb
+++ db/migrate/20191003170132_create_locations.rb
@@ -3,8 +3,8 @@ class CreateLocations < ActiveRecord::Migration[6.0]
     create_table :locations do |t|
       t.string :name, null: false
       t.string :state, null: false
-      t.int :zip, null: false
-      t.int :highway, null: true
+      t.integer :zip, null: false
+      t.integer :highway, null: true
       t.text :description, null: true
       t.timestamps
     end
