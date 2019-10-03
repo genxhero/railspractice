a6a291430ea8ff547ec6e594d5fda2f47e469a00
genxhero <genxhero@users.noreply.github.com>
2019-10-03 11:54:49 -0700

    add Recommendations model

---
+++ db/migrate/20191003185233_create_recommendations.rb
@@ -0,0 +1,13 @@
+class CreateRecommendations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recommendations do |t|
+      t.integer :user_id, null: false
+      t.integer :place_id, null: false
+      t.string :place_type, null: false
+      t.string :message, , null: true
+      t.timestamps
+    end
+    add_index :recommendations, :user_id
+    add_index :recommendations, :place_id
+  end
+end
