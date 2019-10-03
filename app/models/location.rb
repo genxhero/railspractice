01eaf95cad6806f99e5b56b5d79c97081002fd3c
genxhero <genxhero@users.noreply.github.com>
2019-10-03 10:46:12 -0700

    add has many association to location

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,9 @@ class Location < ApplicationRecord
     validates :zip, presence: true
     validates :name, uniqueness:  { scope: :zip,
     message: "only one town exists by that name within #{zip}" }
+
+    has_many :travel_centers, dependent: :destroy,
+    primary_key :id,
+    foreign_key :location_id,
+    class_name "TravelCenter"
 end
--- db/migrate/20191003173801_create_travel_centers.rb
+++ db/migrate/20191003173801_create_travel_centers.rb
@@ -1,13 +1,14 @@
 class CreateTravelCenters < ActiveRecord::Migration[6.0]
   def change
     create_table :travel_centers do |t|
-      t.integer :location_id
+      t.integer :location_id, null: false
       t.string :name
       t.text :description
       t.integer :unleaded
       t.integer :diesel
-
       t.timestamps
     end
+
+    add_index :travel_centers, :location_id
   end
 end
