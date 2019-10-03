3f46f76ce88bc68ef246fef402dc48cebfed1012
genxhero <genxhero@users.noreply.github.com>
2019-10-03 11:47:11 -0700

    remove useless code

--- app/models/travel_center.rb
+++ app/models/travel_center.rb
@@ -1,7 +1,6 @@
 class TravelCenter < ApplicationRecord
     validates :name, presence: true
     validates :location_id, presence: true
-    # validate self.unique_per_location
     validates :name, uniqueness: { scope: :location_id,
     message: "record already exists " }
 
@@ -10,11 +9,4 @@ class TravelCenter < ApplicationRecord
     foreign_key: :location_id,
     class_name: "Location"
 
-    # private 
-
-    # def unique_per_location
-    #     if self.class.exists?(:location_id => location_id, :name => name)
-    #         errors.add :travel_center, "#{name} already exists"
-    #     end
-    # end
 end
