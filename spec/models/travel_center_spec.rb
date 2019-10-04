4eafd6761551495f17636fc35212384f7240afac
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-04 10:46:09 -0700

    add another test

--- spec/models/travel_center_spec.rb
+++ spec/models/travel_center_spec.rb
@@ -10,4 +10,11 @@ describe TravelCenter do
         nowhere = TravelCenter.new(name: "Nowhere", location_id: nil, description: "Nondescript")
         expect(nowhere).to_not be_valid 
     end
+
+    it "will not save if the same name has been used for the same location" do 
+        dupe = TravelCenter.new(name: "Love's Travel Stop", location_id: 1, description: "this is a dupe")
+        expect(dupe).to_not be_valid
+        expect(dupe.errors).to_not be_empty
+    end
+
 end
