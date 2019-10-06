2a17df7454489ebaf205c1b2e84f88783e34c6ed
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-06 14:33:02 -0700

    associations for restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,16 @@
 class Restaurant < ApplicationRecord
+    validates :name, presence: true
+    validates :location_id, presence: true
+    validates :name, uniqueness: { scope: :location_id,
+    message: "record already exists " }
+
+    belongs_to :location,
+    primary_key: :id, 
+    foreign_key: :location_id,
+    class_name: "Location"
+
+    has_many :recommendations, as: :place, dependent: :destroy,
+    primary_key: :id,
+    foreign_key: :place_id,
+    class_name: "Recommendation"
 end
