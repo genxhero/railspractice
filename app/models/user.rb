cb2a4b9fabecf6bda62f3d420453db178d4220ca
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-05 19:56:39 -0700

    add password setter

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,11 @@ class User < ApplicationRecord
     foreign_key: :user_id,
     class_name: "Location"
 
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
     def ensure_token
         self.session_token ||= SecureRandom::urlsafe_base64
     end
