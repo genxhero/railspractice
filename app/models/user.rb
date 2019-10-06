90c413d5e464652ea01042890abed28bd278bfd4
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-05 19:54:12 -0700

    add self.findbycredentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    after_initialize :ensure_token
+
     validates :username, :email, presence: true
     has_many :recommendations,
     primary_key: :id,
@@ -9,4 +11,13 @@ class User < ApplicationRecord
     primary_key: :id, 
     foreign_key: :user_id,
     class_name: "Location"
+
+    def ensure_token
+        self.session_token ||= Urlsafe
+    end
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        user && user.correct_password?(password) ? user: nil
+    end
 end
