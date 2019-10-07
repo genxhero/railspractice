b9c695ede61f884cccd613fa30f5dadadf8f1357
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-07 09:36:07 -0700

    change splash to welcome

--- app/javascript/components/welcome.jsx
+++ app/javascript/components/welcome.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Splash = () => {
+const Welcome = () => {
     return (
         <div>
             <h1>React Rails is the Bomb</h1>
@@ -8,4 +8,4 @@ const Splash = () => {
     )
 }
 
-export default Splash;
+export default Welcome;
