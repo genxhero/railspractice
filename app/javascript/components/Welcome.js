26b32a1364ed2291dc44265718b6e0d2ed15b6ac
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-07 10:07:30 -0700

    add missing dependencies

---
+++ app/javascript/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Welcome extends React.Component {
+  render () {
+    return (
+      <div>
+          <h1> React Rails is Working just fine</h1>
+      </div>
+    )
+  }
+}
+
+export default Welcome
