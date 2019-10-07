327b6e6e73e6d5dabd3d79317a307b0784bc6b2a
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-07 10:21:17 -0700

    enable webpacker

--- app/javascript/components/Welcome.js
+++ app/javascript/components/Welcome.js
@@ -1,12 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
-
 class Welcome extends React.Component {
   render () {
     return (
-      <div>
-          <h1> React Rails is Working just fine</h1>
-      </div>
+      <React.Fragment>
+        <h1>buttsecks</h1>
+      </React.Fragment>
     )
   }
 }
