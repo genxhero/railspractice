2d2db587920ea89cf89ca30d578cffef98c566f5
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-07 21:11:21 -0700

    split welcome message into two lines

--- app/javascript/components/Welcome.js
+++ app/javascript/components/Welcome.js
@@ -4,7 +4,8 @@ class Welcome extends React.Component {
   render () {
     return (
       <div className="welcome-splash">
-        <h1 className="welcome-title">Wanderer - Redux</h1>
+        <h2 className="welcome-subtitle">Welcome To</h2>
+        <h1 className="welcome-title">Wanderer Reborn</h1>
       </div>
     )
   }
