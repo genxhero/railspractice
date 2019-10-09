873398daf103816c40ed9483b68067a5a6199017
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-09 12:40:48 -0700

    get the V portion of MVC to work

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,6 @@
 class LocationsController < ApplicationController
     def show
-        ass = "balls"
-        debugger
-        @location  = Location.find_by(url_key, params[:id])
+        @location  = Location.find_by(url_key: params[:id])
         render :show
     end
 end
