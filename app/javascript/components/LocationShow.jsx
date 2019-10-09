488fe608db0ea3eae68dc9e0daab2fac4c8e3b64
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-09 10:19:33 -0700

    location component and routes

--- app/javascript/components/LocationShow.jsx
+++ app/javascript/components/LocationShow.jsx
@@ -1,4 +1,18 @@
 
 /**
-Page for displaying a specific place
+ * Page for displaying a specific town
  */
+
+import React from "react"
+import PropTypes from "prop-types"
+
+const Location = (props) => {
+    debugger;
+    return (
+        <div>
+            <h1>this is  location</h1>
+        </div>
+    )
+}
+
+export default Location;
