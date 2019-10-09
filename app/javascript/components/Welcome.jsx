813a80dd11bb396bb67103746f13dc7bc04885e0
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-08 18:24:30 -0700

    quick

--- app/javascript/components/Welcome.jsx
+++ app/javascript/components/Welcome.jsx
@@ -4,8 +4,10 @@ class Welcome extends React.Component {
   render () {
     return (
       <div className="welcome-page">
-        <h2 className="welcome-subtitle">Welcome To</h2>
-        <h1 className="welcome-title">Wanderer Reborn</h1>
+        <div className="welcome-message">
+          <h2 className="welcome-subtitle">Welcome To</h2>
+          <h1 className="welcome-title">Wanderer Reborn</h1>
+        </div>
       </div>
     )
   }
