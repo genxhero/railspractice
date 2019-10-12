f4c10803718e7957379fd4f733cad8f343b934eb
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-12 10:34:53 -0700

    add shop_type to restaurants

--- db/migrate/20191012172734_removetypecolumnfromrestaurants.rb
+++ db/migrate/20191012172734_removetypecolumnfromrestaurants.rb
@@ -1,4 +1,6 @@
 class Removetypecolumnfromrestaurants < ActiveRecord::Migration[6.0]
   def change
+    remove_column :restaurants, :type
+    add_column :restaurant, :shop_type, :string, default: 'Unspecified'
   end
 end
