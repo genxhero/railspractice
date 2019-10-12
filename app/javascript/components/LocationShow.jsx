85cbc7431d482fc6aacd288034159c0966b7b902
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-11 22:39:56 -0700

    add travel center component to show page

--- app/javascript/components/LocationShow.jsx
+++ app/javascript/components/LocationShow.jsx
@@ -5,14 +5,25 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import TravelCenter from './TravelCenter';
 
 const Location = (props) => {
-    const buffer = "this";
     debugger;
-    const ass = "butt";
     return (
-        <div>
-            <h1>this is  location</h1>
+        <div className="location-show-page">
+            <div className="location-show columns">
+                <div>
+                    Insert coffee shops here
+                </div>
+
+                <div>Insert Restaurants here</div>
+
+                <div>
+                    {props.locations.travelCenters.map( travelCenter => {
+                        return <TravelCenter travelCenter={travelCenter} />
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
