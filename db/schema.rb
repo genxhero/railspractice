b849c00649426137f5b5a8e4a068280b027d5984
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-12 10:19:52 -0700

    fix other noob mistake in migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_173528) do
+ActiveRecord::Schema.define(version: 2019_10_12_060039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,15 +19,14 @@ ActiveRecord::Schema.define(version: 2019_10_09_173528) do
     t.string "name", null: false
     t.integer "location_id", null: false
     t.string "description", null: false
-    t.string "type", null: false
     t.boolean "couches", null: false
     t.boolean "armchairs", null: false
     t.boolean "restrooms", null: false
     t.boolean "wifi", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "shop_type", default: "Unspecified"
     t.index ["location_id"], name: "index_coffee_shops_on_location_id"
-    t.index ["type"], name: "index_coffee_shops_on_type"
   end
 
   create_table "locations", force: :cascade do |t|
