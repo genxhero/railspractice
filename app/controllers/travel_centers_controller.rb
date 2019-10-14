638f72406fa5028bd4c594df680379f50057bf2e
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-14 12:46:35 -0700

    add travel center controller

---
+++ app/controllers/travel_centers_controller.rb
@@ -0,0 +1,20 @@
+class TravelCentersController > ApplicationController
+    def new
+        @travel_center = TravelCenter.new
+        render :new 
+    end
+
+    def create
+        @travel_center = TravelCenter.new(travel_center_params)
+        if @travel_center.save
+          render 
+        else
+           render json: @travel_center.errors.full_messages, status: 422
+        end
+      end
+
+    private
+    def travel_center_params
+      params.require(:travel_center).permit(:location_id, :name, :description, :unleaded, :diesel)
+    end
+end
