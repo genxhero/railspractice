e34a7a01c841afa8e7b009ba5eb838e064849e32
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-14 16:30:48 -0700

    add inputs to fields

--- app/javascript/components/TravelCenterCreate.jsx
+++ app/javascript/components/TravelCenterCreate.jsx
@@ -1,9 +1,27 @@
 import React, {useState}from 'react';
-const TravelCenterCreate = () => {
+
+const TravelCenterCreate = (props) => {
+
+    const handleInputChange = e => {
+        const {name, value} = e.target
+        setValues({...values, [name]: value})
+    }
+
+    const [values, setValues] = useState({name: '', description: '', unleaded: 0, diesel: 0})
+    console.log(values);
     return (
         <div className="place-form-page">
             <div className="place-form">
-               
+               <input 
+                 name='name'
+                 type="text"
+                 onChange={handleInputChange}
+                 value={values.name}/>
+               <input 
+                 type='text' 
+                 name='description'
+                 onChange={handleInputChange}
+                 value={values.name}/>
             </div>
         </div>
     )
