9fb162bf37ab7fc707297f1f1e841557d8946b97
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-13 22:49:14 -0700

    doc comment for coffee shop component

--- app/javascript/components/CoffeeShop.jsx
+++ app/javascript/components/CoffeeShop.jsx
@@ -1,8 +1,13 @@
+/**
+ * This component displays all information for a gven coffee shop.
+ * It expects a JSON object as a prop.
+ */
+
 import React from 'react'
 
 const CoffeeShop = (props) => {
     return (
-        <div>
+        <div className="place-card">
             {props.coffeeShop.name}
         </div>
     )
