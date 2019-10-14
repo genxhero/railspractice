5b0207058a071dee2b9e940bb2f588f3c157cfa3
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-14 16:23:26 -0700

    import the hooks

---
+++ app/javascript/components/TravelCenterCreate.jsx
@@ -0,0 +1,11 @@
+import React, {useState}from 'react';
+const TravelCenterCreate = () => {
+    return (
+        <div className="place-form-page">
+            <div className="place-form">
+               
+            </div>
+        </div>
+    )
+}
+export default TravelCenterCreate;
