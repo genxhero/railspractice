d5b1fe3b7bdf3de2e9ff8cf900eb3dae4fa3d123
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-13 22:51:18 -0700

    doc comment for travel center component

--- app/javascript/components/TravelCenter.jsx
+++ app/javascript/components/TravelCenter.jsx
@@ -1,8 +1,12 @@
+/**
+ * Component to display a Travel center
+ */
+
 import React from 'react'
 
 const TravelCenter = (props) => {
     return (
-        <div>
+        <div className="place-card">
             {props.travelCenter.name} 
         </div>
     )
