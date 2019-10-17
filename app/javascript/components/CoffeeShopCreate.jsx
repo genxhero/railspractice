38e61bf9dee91a17031ec44a620f6e8088d5ce6a
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-16 22:26:29 -0700

    build coffee shop creation form

---
+++ app/javascript/components/CoffeeShopCreate.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+
+const CoffeeShopCreate = (props) => {
+    const handleInputChange = e => {
+        const {name, value} = e.target
+        setValues({...values, [name]: value})
+    }
+
+    const [values, setValues] = useState({name: '', description: '', shopType: '', couches: false, armchairs: false, wifi: false})
+    return (
+        <div className="place-form-modal">
+            <div className="place-form">
+                <h2>New Coffee Shop</h2>
+               <input 
+                 className="place-form-text"
+                 name='name'
+                 type="text"
+                 placeholder="Name"
+                 onChange={handleInputChange}
+                 value={values.name}/>
+                <input 
+                 className="place-form-text"
+                 name='cuisine'
+                 type="text"
+                 placeholder="Cuisine"
+                 onChange={handleInputChange}
+                 value={values.cuisine}/>
+               <textarea 
+                 className="place-form-text"
+                 type='text' 
+                 name='description'
+                 placeholder="Description"
+                 onChange={handleInputChange}
+                 value={values.description}/>
+                <div>
+                    <button className="place-form-btn"onClick={props.close} >Cancel</button>
+                </div>
+        </div>
+    </div>
+    );
+}
+
+export default CoffeeShopCreate;
