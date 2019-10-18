71c9ce4fad9227e0ba0f9342ea83c82344915b41
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-18 11:35:28 -0700

    migrated the database.

--- db/migrate/20191018183358_change_unleaded_and_diesel_to_floats.rb
+++ db/migrate/20191018183358_change_unleaded_and_diesel_to_floats.rb
@@ -1,4 +1,6 @@
 class ChangeUnleadedAndDieselToFloats < ActiveRecord::Migration[6.0]
   def change
+    change_column :travel_centers, :unleaded, :float
+    change_column :travel_centers, :diesel, :float
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_180517) do
+ActiveRecord::Schema.define(version: 2019_10_18_183358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,8 +71,8 @@ ActiveRecord::Schema.define(version: 2019_10_12_180517) do
     t.integer "location_id", null: false
     t.string "name"
     t.text "description"
-    t.integer "unleaded"
-    t.integer "diesel"
+    t.float "unleaded"
+    t.float "diesel"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["location_id"], name: "index_travel_centers_on_location_id"
