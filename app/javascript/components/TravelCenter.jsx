4a686c9e7ad4b66a4d43ad66a238ae576b960be7
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-18 12:01:23 -0700

    add plus and minus to cards

--- app/javascript/components/TravelCenter.jsx
+++ app/javascript/components/TravelCenter.jsx
@@ -3,6 +3,7 @@
  */
 
 import React, {useState} from 'react'
+import { FaPlus, FaMinus } from "react-icons/fa";
 
 
 const TravelCenter = (props) => {
@@ -11,14 +12,18 @@ const TravelCenter = (props) => {
     if (isExpanded) {
     return (
             <div className="place-card">
-                {props.travelCenter.name} 
-                <div>Unleaded: ${parseFloat(props.travelCenter.unleaded).toFixed(2)}</div>
-                <div>Diesel: ${parseFloat(props.travelCenter.diesel).toFixed(2)}</div>
+                <FaMinus onClick ={() => expandOrCollapse(false)}/>
+                <div>
+                    {props.travelCenter.name} 
+                    <div>Unleaded: ${parseFloat(props.travelCenter.unleaded).toFixed(2)}</div>
+                    <div>Diesel: ${parseFloat(props.travelCenter.diesel).toFixed(2)}</div>
+                </div>
             </div>
         )
     } else {
         return (
-            <div className="place-card"onClick={() => expandOrCollapse(true)}>
+            <div className="place-card">
+                <FaPlus onClick={() => expandOrCollapse(true)} /> 
                 {props.travelCenter.name}
             </div>
         )
