b51552df692531765f1b340014a61e4541d587aa
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-18 11:50:26 -0700

    add expand functionality to travel stops

--- app/javascript/components/TravelCenter.jsx
+++ app/javascript/components/TravelCenter.jsx
@@ -2,16 +2,29 @@
  * Component to display a Travel center
  */
 
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const TravelCenter = (props) => {
+    const [isExpanded, expandOrCollapse] = useState(false)
+
+    if (isExpanded) {
     return (
-        <div className="place-card">
-            {props.travelCenter.name} 
-            <div>Unleaded: ${parseFloat(props.travelCenter.unleaded).toFixed(2)}</div>
-            <div>Diesel: ${parseFloat(props.travelCenter.diesel).toFixed(2)}</div>
-        </div>
-    )
+            <div className="place-card">
+                {props.travelCenter.name} 
+                <div>Unleaded: ${parseFloat(props.travelCenter.unleaded).toFixed(2)}</div>
+                <div>Diesel: ${parseFloat(props.travelCenter.diesel).toFixed(2)}</div>
+            </div>
+        )
+    } else {
+        return (
+            <div onClick={() => expandOrCollapse(true)}>
+                EXPAND DONG
+            </div>
+        )
+    }
+
+   
 }
 
 export default TravelCenter;
