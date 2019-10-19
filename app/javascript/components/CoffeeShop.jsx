fe5f49ab3ab87d4aff9490b4473365190e494690
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-19 14:49:37 -0700

    wrap card data in a div for coffee shops

--- app/javascript/components/CoffeeShop.jsx
+++ app/javascript/components/CoffeeShop.jsx
@@ -3,15 +3,20 @@
  * It expects a JSON object as a prop.
  */
 
-import React from 'react'
+import React, {useState} from 'react'
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+
 
 const CoffeeShop = (props) => {
     return (
         <div className="place-card">
-            <div>{props.coffeeShop.name}</div>
-            <div>WiFi: {props.coffeeShop.wifi ? "Yes" : "No"}</div>
-            <div>Couches: {props.coffeeShop.couches ? "Yes" : "No"} </div>
-            <div>Armchairs: {props.coffeeShop.armchairs ? "Yes" : "No"}</div>
+            <div className="place-card-data">
+                <div>{props.coffeeShop.name}</div>
+                <div>WiFi: {props.coffeeShop.wifi ? "Yes" : "No"}</div>
+                <div>Couches: {props.coffeeShop.couches ? "Yes" : "No"} </div>
+                <div>Armchairs: {props.coffeeShop.armchairs ? "Yes" : "No"}</div>
+            </div>
+       
         </div>
     )
 }
