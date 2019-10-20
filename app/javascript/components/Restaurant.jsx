73c56e591262b43855b82941ff7a065a509d4e68
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-19 20:32:08 -0700

    add collapsed restaurants

--- app/javascript/components/Restaurant.jsx
+++ app/javascript/components/Restaurant.jsx
@@ -10,17 +10,29 @@ const Restaurant = (props) => {
 
     if (isExpanded) {
          return (
-        <div className="place-card-expanded">
-            <div className="chevron-container">
-                    <FaChevronUp onClick ={() => expandOrCollapse(false)}/>
+            <div className="place-card-expanded">
+                <div className="chevron-container">
+                        <FaChevronUp onClick ={() => expandOrCollapse(false)}/>
+                    </div>
+                <div className="place-card-data">
+                    <div>{props.restaurant.name}</div>
+                    <div>Type: {props.restaurant.shopType}</div>
                 </div>
-            <div className="place-card-data">
-                <div>{props.restaurant.name}</div>
-                <div>Type: {props.restaurant.shopType}</div>
+        
             </div>
-       
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="place-card-collapsed">
+                <div className="chevron-container">
+                    <FaChevronDown onClick={() => expandOrCollapse(true)} /> 
+                </div>
+                <div className="place-card-data">
+                Name will go here later.
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Restaurant;
