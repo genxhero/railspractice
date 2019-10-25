e9af6e97c2a912d5a6ef589591b58082fac40ee5
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-24 23:38:52 -0700

    todo for welcome

--- app/javascript/components/Welcome.jsx
+++ app/javascript/components/Welcome.jsx
@@ -2,6 +2,8 @@
 /**
  * The first thing a user will see, this splash page will be the main portal.
  * Should contain links to the map or search feature.
+ * Additionally, a search bar for finding places that match.
+ * Actually a dropdown menu for states and highways could be awesome.
  */
 import React from "react"
 import PropTypes from "prop-types"
@@ -13,6 +15,7 @@ class Welcome extends React.Component {
           <h2 className="welcome-subtitle">Welcome To</h2>
           <h1 className="welcome-title">Wanderer Reborn</h1>
         </div>
+        <div className="Welcome search bar"></div> 
       </div>
     )
   }
