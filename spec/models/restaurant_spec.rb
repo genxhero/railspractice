d1a79749e077964ce1bdf0d653cd727270792e8e
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-24 23:44:26 -0700

    add test

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is invalid if the same name has been used for the same location" do 
+    dupe = Restaurant.new(name: "Bob's Burgers", location_id: 1, description: "this is a dupe")
+    expect(dupe).to_not be_valid
+end
 end
