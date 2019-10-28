50c38e0d7f105f3718e9570043fe73e62e333826
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-28 10:35:02 -0700

    make a basic error modal

---
+++ app/javascript/components/ErrorModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorModal = (props) => {
+debugger;
+    return (
+        <div className="error-modal">
+            {props.errors}
+        </div>
+    )
+
+}
+
+export default ErrorModal
