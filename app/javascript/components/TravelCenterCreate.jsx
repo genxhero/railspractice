0a8fa79568bb405491fd38c570d884a9f4ae2886
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-28 10:39:38 -0700

    import modal into creation form

--- app/javascript/components/TravelCenterCreate.jsx
+++ app/javascript/components/TravelCenterCreate.jsx
@@ -7,6 +7,7 @@
  */
 
 import React, {useState}from 'react';
+import ErrorModal from './ErrorModal';
 import axios from 'axios'
 
 const TravelCenterCreate = (props) => {
@@ -79,7 +80,7 @@ const TravelCenterCreate = (props) => {
                 </div>
               
             </div>
-          
+          {errors && <Errormodal errors={errors}/>}
         </div>
     )
 }
