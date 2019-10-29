7884713ecb4d0ce5ed51709b7309a30f1c3066f9
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-29 09:12:29 -0700

    restyle the clear button

--- app/javascript/components/ErrorModal.jsx
+++ app/javascript/components/ErrorModal.jsx
@@ -4,13 +4,14 @@ const ErrorModal = (props) => {
     return (
         <div className="error-modal">
             <div className="error-message-area">
+                <h3>The Following Errors Occured</h3>
                 {props.errors.map( error => {
                     return (
                         <div key={error}className="error-message">{error}</div>
                     )
                 })
             }
-                <button className="error-clear-btn" onClick={props.clear}>Clear</button>
+                <button className="error-clear-btn" onClick={props.clear}>CLEAR</button>
             </div>  
         </div>
     )
