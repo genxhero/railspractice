71887d3502571cada8abc4c5f1927189685c5e0a
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-31 12:10:52 -0700

    safety commit prior to migration

--- db/migrate/20191031185626_createlodgingtable.rb
+++ db/migrate/20191031185626_createlodgingtable.rb
@@ -6,6 +6,10 @@ class Createlodgingtable < ActiveRecord::Migration[6.0]
       t.integer :location_id, null: false
       t.string :lodging_type, null: false
       t.string :breakfast, null: false
+      t.boolean :pool, null: false
+      t.boolean :gym, null: false
+      t.string :cable_channels, array: true, default: []
+      t.string :wifi, null: false
       t.timestamps
     end
     add_index :lodgings, :location_id
