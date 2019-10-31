4057d3f213bd9d145417656b22bf144ac3fdfeaa
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-31 11:46:58 -0700

    add restaurant controller

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,22 @@
+class RestaurantsController < ApplicationController
+    # Temporary until i figure out the rack cors problem
+    skip_before_action :verify_authenticity_token
+  
+  
+      def create
+          @restaurant = TravelCenter.new(restaurant_params)
+          if @restaurant.save
+            @location = Location.find(restaurant_params[:location_id])
+            render json: @restaurant
+            # render 'locations/show' 
+          else
+             render json: @restaurant.errors.full_messages, status: 422
+          end
+      end
+  
+      private
+      def restaurant_params
+        params.require(:restaurant).permit(:location_id, :name, :description, :unleaded, :diesel)
+      end
+  
+    end
