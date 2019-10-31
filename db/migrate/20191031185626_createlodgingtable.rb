065e59ee4759f2283ec82d9c1dfd82e0da50046a
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-31 12:02:14 -0700

    write up the migration

--- db/migrate/20191031185626_createlodgingtable.rb
+++ db/migrate/20191031185626_createlodgingtable.rb
@@ -1,4 +1,14 @@
 class Createlodgingtable < ActiveRecord::Migration[6.0]
   def change
+    create_table :lodgings do |t|
+      t.string :name, null: false
+      t.text :description, null: true
+      t.integer :location_id, null: false
+      t.string :lodging_type, null: false
+      t.string :breakfast, null: false
+      t.timestamps
+    end
+    add_index :lodgings, :location_id
+    add_index :lodgings, :name
   end
 end
