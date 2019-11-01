ba2c7139b4b27f92b51626d6ed58806cf341a180
Aaron Goddard <genxhero@users.noreply.github.com>
2019-10-31 19:20:10 -0700

    add restaurants, lodgings, and coffee shops to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :welcome, only: [:index]
   resources :locations
   resources :travel_centers
+  resources :restaurants
+  resources :lodgings
+  resources :coffee_shops
 end
