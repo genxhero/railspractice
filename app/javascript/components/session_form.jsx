a2061ea26fba04e4af6e59aebb1d8c589a312f0a
Aaron Goddard <genxhero@users.noreply.github.com>
2019-11-13 22:57:40 -0800

    Add usestate hook

--- app/javascript/components/session_form.jsx
+++ app/javascript/components/session_form.jsx
@@ -3,11 +3,12 @@
  *  formType: String; one of "Login" or "Register"
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const SessionForm = (props) => {
     console.log("Pointing")
+    const [values, setValues] = useState({})
     return (
         <div className="session-modal">
             <div className="session-form">
