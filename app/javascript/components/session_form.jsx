4d88e56be6d1550a363a42f1064ce6a17c17da1b
Aaron Goddard <genxhero@users.noreply.github.com>
2019-11-13 22:57:27 -0800

    create basic div stucture for session form

--- app/javascript/components/session_form.jsx
+++ app/javascript/components/session_form.jsx
@@ -2,3 +2,17 @@
  * Props
  *  formType: String; one of "Login" or "Register"
  */
+
+import React from 'react';
+import axios from 'axios';
+
+const SessionForm = (props) => {
+    console.log("Pointing")
+    return (
+        <div className="session-modal">
+            <div className="session-form">
+
+            </div>
+        </div>
+    )
+}
