49adb02583e346582fe694ad56d3f5a674f17166
Aaron Goddard <genxhero@users.noreply.github.com>
2019-11-29 13:14:34 -0800

    add current user

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,7 @@
 class LocationsController < ApplicationController
     def show
         @location  = Location.eager_load([:coffee_shops, :restaurants, :travel_centers]).find_by(url_key: params[:id])
+        @current_user = current_user
         render :show
     end
 end
