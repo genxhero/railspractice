d24ded0129efc534a5c7da7fc3297231024528e0
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-01 12:21:23 -0800

    wrap session buttons in a ternary

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -6,17 +6,26 @@ import Login from './Login';
 
 const Header = props => {
     console.log(props)
-
+    const [currentUser, updateUser] = useState(props.currentUser);
     const [form, setForm] = useState({open: false, type: ""})
     const close = () =>{  setForm({open:false, type: ""});    }
 
     return (
         <div className="header">
             <h1> This is a header</h1>
-            <button onClick={ () => { setForm({open: true, type: "login"})}}>Login</button>
-            <button onClick={ () => { setForm({open: true, type: "register"})}}>Register</button>
-            {(form.open && form.type === "login") && <Login close={close} />}
-            {(form.open && form.type === "register")  && <Signup close={close}/>}
+            {
+                currentUser ? (
+                <div>
+                    <button onClick={ () => { setForm({open: true, type: "login"})}}>Login</button>
+                    <button onClick={ () => { setForm({open: true, type: "register"})}}>Register</button>
+                </div> 
+                ) : (
+                    <h3>currentUser.username</h3>
+                )
+            }
+      
+            {(form.open && form.type === "login") && <Login close={close} update={updateUser}/>}
+            {(form.open && form.type === "register")  && <Signup close={close} update={updateUser}/>}
         </div>
     )
 }
