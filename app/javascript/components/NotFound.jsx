23334bf4ce0f1b0d4a6e3f896dd60545bfab93f6
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-12 18:33:54 -0800

    more documentation

--- app/javascript/components/NotFound.jsx
+++ app/javascript/components/NotFound.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+/**
+ * Presents a "not found" page which will contain a link to the index page and a polite message
+ */
+
 const NotFound = (props) => {
     const {pageType, identifier} = props;
     return <div style={{"position":"relative", "top":"25rem"}}>
--- app/javascript/components/utils.js
+++ app/javascript/components/utils.js
@@ -1,3 +1,7 @@
+/**
+ * Creates a unique GUID on the front end for test groups.
+ */
+
 export const assignTestingGuid = () => {
     let guid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     localStorage.setItem("testGuid", guid)
