90b29d0424cb51e4d4782157ad63fd302bacd961
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-14 11:43:09 -0800

    add personal info to the dashboard to test props working

--- app/javascript/components/UserDashboard.jsx
+++ app/javascript/components/UserDashboard.jsx
@@ -9,6 +9,8 @@ const UserDashboard = (props) => {
     const {user, currentUser} = props; 
     return(
         <div>
+            <h1>Welcome, {user.username}</h1>
+            <h2>Your email: {user.email}</h2>
             This is a user page. Deal with it.
         </div>
     )
