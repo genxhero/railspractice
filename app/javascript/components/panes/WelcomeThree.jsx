7f9647eb87cee7f3717617b7df38d097cb14532c
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-13 21:20:35 -0800

    write up a rough draft for app panel

--- app/javascript/components/panes/WelcomeThree.jsx
+++ app/javascript/components/panes/WelcomeThree.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const WelcomeThree = () =>{
-
+    return (
+        <div className="welcome-pane">
+            <h2>Road Trips Made Simple</h2>
+            <p>Wanderer makes it easy to find</p>
+            <div>
+                <li>Travel Centers</li>
+                <li>Coffee Shops</li>
+                <li>Lodging</li>
+                <li>Restaurants</li>
+            </div>
+            <p>Recommendations from adventurers like you!</p>
+        </div>
+    )
 }
 
 export default WelcomeThree;
