7f6aa2d5b67b11df589a18eca66b9d1adae8349d
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-13 21:25:51 -0800

    about panel

--- app/javascript/components/panes/WelcomeFour.jsx
+++ app/javascript/components/panes/WelcomeFour.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const WelcomeFour = () => {
     return (
         <div className="welcome-pane">
-            
+            <h3>About</h3>
+            <li>           
+                 <p>Wanderer Reborn is built with a Ruby on Rails backend using a PostgreSQL database and a React front-end loaded with the react-rails gem</p>
+            </li>
+            <li>
+                <p>
+                    
+                </p>
+            </li>
         </div>
     )
 }
