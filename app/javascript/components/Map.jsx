b2f64bcede028b069a9585b19ec56a7a371a849e
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-23 08:10:20 -0800

    start the map component

---
+++ app/javascript/components/Map.jsx
@@ -0,0 +1,8 @@
+import React, {useState} from 'react'
+import {Link, withRouter} from 'react-router-dom';
+
+const Map = (props) => {
+
+}
+
+export default withRouter(Map);
---
+++ app/javascript/utils/maps_utils.js
@@ -0,0 +1,3 @@
+/**
+ * This is where all functions pertaining to Google Maps lives.
+ */
