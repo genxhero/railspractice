460cfd7d33ceb2edbc3c2420e9e91f68bc548761
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-23 10:38:08 -0800

    set default props for map

--- app/javascript/components/Map.jsx
+++ app/javascript/components/Map.jsx
@@ -1,8 +1,24 @@
 import React, {useState} from 'react'
 import {Link, withRouter} from 'react-router-dom';
+import GoogleMapReact from 'google-map-react';
 
 const Map = (props) => {
-
+return (
+    <GoogleMapReact
+    bootstrapURLKeys={{ key:""}}
+    defaultCenter={this.props.center}
+    defaultZoom={this.props.zoom}
+  >
+       </GoogleMapReact>
+)
 }
+
+Map.defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
 
 export default withRouter(Map);
