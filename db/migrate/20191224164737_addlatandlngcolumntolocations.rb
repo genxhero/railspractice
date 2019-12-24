7835fa3a74e51dfa5df590aa1b2b1355446b5ac4
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-24 08:50:33 -0800

    write up the migraton

--- db/migrate/20191224164737_addlatandlngcolumntolocations.rb
+++ db/migrate/20191224164737_addlatandlngcolumntolocations.rb
@@ -1,4 +1,6 @@
 class Addlatandlngcolumntolocations < ActiveRecord::Migration[6.0]
   def change
+    add_column :locations, :lat, :decimal, {:precision=>10, :scale=>6}
+    add_column :locations, :lng, :decimal, {:precision=>10, :scale=>6}
   end
 end
