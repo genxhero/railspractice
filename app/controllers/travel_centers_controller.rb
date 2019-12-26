837e05ab29e876010816b1db7f8b5068866233fb
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-26 13:59:50 -0800

    jsonify the result of travel center

--- app/controllers/travel_centers_controller.rb
+++ app/controllers/travel_centers_controller.rb
@@ -14,7 +14,16 @@ class TravelCentersController < ApplicationController
         @travel_center = TravelCenter.new(travel_center_params)
         if @travel_center.save
           @location = Location.find(travel_center_params[:location_id])
-          render json: @travel_center
+          render json:   {
+            fields: [
+                {name: "Unleaded", value: "$#{@travel_center.unleaded}#{(@travel_center.unleaded * 100) % 10 == 0 ? "0" : ""}"},
+                {name: "Diesel", value: "$#{@travel_center.diesel}#{(@travel_center.diesel * 100) % 10 == 0 ? "0" : ""}"}
+            ],
+            name: @travel_center.name,
+            description: @travel_center.description,
+            lat: @travel_center.lat,
+            lng: @travel_center.lng
+        }
           # render 'locations/show' 
         else
            render json: @travel_center.errors.full_messages, status: 422
