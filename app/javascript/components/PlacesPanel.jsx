5fc792f577cb085a84a3c606f710cb3f984b4a25
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-26 10:35:23 -0800

    css for panel

--- app/javascript/components/PlacesPanel.jsx
+++ app/javascript/components/PlacesPanel.jsx
@@ -1,13 +1,39 @@
 import React, {useState} from 'react';
 
 const PlacesPanel = (props) => {
-    const [activePanel, setPanel] = useState()
+    const [activePanel, setPanel] = useState("travelCenters")
+    const selectPane = (e) => {
+
+    }
     return (
         <div className="places-panel">
             <div className="places-nav">
-             <div className={`places-tab${activePanel === 'Travel Centers' ? '-active' : 'inactive'}`}>
+             <div className={`places-tab${activePanel === 'travelCenters' ? '-active' : 'inactive'}`} 
+                  onClick={selectPane}
+                  name="travelCenters"
+                  >
                 Travel Centers
              </div>
+             <div className={`places-tab${activePanel === 'coffeeShops' ? '-active' : 'inactive'}`}
+              onClick={selectPane}
+              name="coffeeShops"
+             >
+                Coffee Shops
+             </div>
+             <div className={`places-tab${activePanel === 'restaurants' ? '-active' : 'inactive'}`}
+              onClick={selectPane}
+              name="restaurants"
+             >
+                Restaurants
+             </div>
+
+             <div className={`places-tab${activePanel === 'lodgings' ? '-active' : 'inactive'}`}
+              onClick={selectPane}
+              name="lodgings"
+             >
+                Lodging
+             </div>
+
             </div>
             <div className="places-panel-content">
 
