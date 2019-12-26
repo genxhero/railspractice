fe329ecfa11e894a288a0660aaa6a136fa8e041a
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-26 09:54:10 -0800

    butt

--- app/javascript/components/Marker.jsx
+++ app/javascript/components/Marker.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 const Marker = (props) => {
+    
     const { color, name, id, key} = props;
+    const showName = () => {
+      console.log(name)
+    }
     return (
       <div className="marker"
         style={{ backgroundColor: color, cursor: 'pointer'}}
         title={name}
         key={key}
+        onClick={showName}
       />
     );
   };
