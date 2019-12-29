5cc81971b49846fd5542c18c421cbb149bae9e50
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-28 18:54:04 -0800

    comment for LocationfInder

--- app/javascript/components/LocationFinder.jsx
+++ app/javascript/components/LocationFinder.jsx
@@ -1,9 +1,14 @@
+/**
+ * Finds locations along highways.
+ * Eventually planning to build a system by which locations may be found
+ * on a route.
+ */
 import React, {useState, useEffect} from 'react';
 import {Link, withRouter} from 'react-router-dom';
 
 const LocationFinder = () => {
     return (
-        <div>
+        <div className="location-finder">
 
         </div>
     )
