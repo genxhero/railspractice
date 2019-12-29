e8660dc11b9b174e09b585aa26859b6b2c37f593
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-28 18:43:27 -0800

    add update center as prop of marker

--- app/javascript/components/Map.jsx
+++ app/javascript/components/Map.jsx
@@ -7,7 +7,7 @@ import Marker from './Marker';
 const Map = (props) => {
     const key = secret;
     const [center, updateCenter] = useState(props.center)
-    console.log(props.zoom)
+    console.log(center)
 return (
     <div className="location-show-map">
         <GoogleMapReact
@@ -15,16 +15,16 @@ return (
           defaultCenter={center}
           defaultZoom={props.zoom}>
            { props.travelCenters.map( place => {
-             return <Marker lat={place.lat} lng={place.lng} color="blue" key={place.name} name={place.name}/>
+             return <Marker lat={place.lat} lng={place.lng} color="blue" key={place.name} name={place.name} centerOnMe={updateCenter}/>
             })}
             { props.restaurants.map( place => {
-             return <Marker lat={place.lat} lng={place.lng} color="red" key={place.name} name={place.name}/>
+             return <Marker lat={place.lat} lng={place.lng} color="red" key={place.name} name={place.name} centerOnMe={updateCenter}/>
             })}
             { props.lodgings.map( place => {
-             return <Marker lat={place.lat} lng={place.lng} color="green" key={place.name} name={place.name}/>
+             return <Marker lat={place.lat} lng={place.lng} color="green" key={place.name} name={place.name} centerOnMe={updateCenter}/>
             })}
             { props.coffeeShops.map( place => {
-             return <Marker lat={place.lat} lng={place.lng} color="brown" key={place.name} name={place.name}/>
+             return <Marker lat={place.lat} lng={place.lng} color="brown" key={place.name} name={place.name} centerOnMe={updateCenter}/>
             })}
                
           </GoogleMapReact>
