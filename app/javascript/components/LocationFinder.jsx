170e446957adac441f51c1439a544239878b6073
Aaron Goddard <genxhero@users.noreply.github.com>
2019-12-28 18:52:56 -0800

    start the location finder component

---
+++ app/javascript/components/LocationFinder.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+
+const LocationFinder = () => {
+    return (
+        <div>
+
+        </div>
+    )
+} 
+
+export default withRouter(LocationFinder);
