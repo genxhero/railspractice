711a482fb2a8e6c3403c99b52eec04da47b93d11
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-03 15:22:50 -0800

    remove useless code

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -2,8 +2,6 @@ class Api::LocationsController < ApplicationController
     def show
         @location  = Location.eager_load([:coffee_shops, :restaurants, :travel_centers]).find_by(url_key: params[:id])
         @user = current_user
-        #render :show 
-        #TODO json jbuilder.
         if @location 
             render json: { location: @location, places: @location.places, currentUser: @user }
         else
